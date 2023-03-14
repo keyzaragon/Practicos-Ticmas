4e665125b893b90fc57ce983eae32444e74adf9a
keyzaragon <125732239+keyzaragon@users.noreply.github.com>
2023-03-14 11:01:38 -0300

    Add files via upload
    versión final

---
+++ comportamientos.js
@@ -0,0 +1,28 @@
+
+
+function imprimir(){
+	window.print('');
+	}
+  
+function accion(){
+    let ancla = document.getElementsByClassName('nav-enlace');
+    for(let i = 0; i < ancla.length; i++){
+      ancla [i].classList.toggle('desaparece');
+    }
+  }
+  
+let txtOculto_btn = document.getElementById('txtOculto_btn');
+
+let txtOculto = document.getElementById('txtOculto');
+
+txtOculto_btn.addEventListener('click', toggleTxt);
+
+function toggleTxt() {
+    txtOculto.classList.toggle ('mostrar');
+    if (txtOculto.classList.contains('mostrar')) {
+        txtOculto_btn.innerHTML = 'No mostrar';
+}
+    else {txtOculto_btn.innerHTML = 'Mostrar';
+    }
+}
+
